d8726a389b657bf523a22641ececf286ed45b965
magalhau <guilherme.magalhaes.grr@gmail.com>
2025-08-24 16:32:14 -0300

    player
    o personagem esta andando, saltando;
    tiles de teste colocados ;
    animaçoes de teste feitas;

---
+++ Brackeys-Game-Jam/Assets/scripts/S_perso/Player_Mov.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Mov : MonoBehaviour
+{
+    private float horizontalInput;
+    private Rigidbody2D rb;
+
+    public int velocity;
+    public float jumpForce;
+
+    [SerializeField] private Transform pedoperso;
+    [SerializeField]private LayerMask groundLayer;
+
+    private bool grounded;
+
+    private Animator Animator;
+    private int movendoHash = Animator.StringToHash("movendo");
+    private int saltandoHash = Animator.StringToHash("saltando");
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();    
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       horizontalInput = Input.GetAxis("Horizontal");
+            
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        grounded = Physics2D.OverlapCircle(pedoperso.position, 0.1f, groundLayer);
+
+        Animator.SetBool(movendoHash, horizontalInput != 0);
+        Animator.SetBool(saltandoHash, !grounded);
+
+        if (horizontalInput > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (horizontalInput < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalInput * velocity, rb.velocity.y);
+    }
+}
