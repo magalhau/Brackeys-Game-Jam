f5d8877cf3790d81575b770556e0aba5fa93a9e1
magalhau <guilherme.magalhaes.grr@gmail.com>
2025-08-25 21:27:24 -0300

    ajuste de bug
    fiz o inimigo passar pelas plataformas do cenario,
    
    toda a vez que vc quizer que ele ignore a cololisao com um item adiciona a tag "ItemIgnorado"

--- Brackeys-Game-Jam/Assets/scripts/Inimigos/Biscoitao/Biscoitao.cs
+++ Brackeys-Game-Jam/Assets/scripts/Inimigos/Biscoitao/Biscoitao.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class Biscoitao : MonoBehaviour
 {
-
     public float speed;
-
-    void Start()
-    {
-        
-    }
 
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Se o objeto tem a tag que você quer ignorar
+        if (collision.gameObject.CompareTag("ItemIgnorado"))
+        {
+            // Pega o colisor do inimigo e do item
+            Collider2D colisorInimigo = GetComponent<Collider2D>();
+            Collider2D colisorItem = collision.collider;
+
+            // Ignora colisão
+            Physics2D.IgnoreCollision(colisorInimigo, colisorItem);
+        }
     }
 }
